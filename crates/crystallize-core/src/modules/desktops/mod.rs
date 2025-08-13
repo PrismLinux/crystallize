58740cad1c96744b5735fcf7118a6380de50966d
Volodia Kraplich <v1mkss.m+git@gmail.com>
2025-08-13 17:24:20 +0300

    changed possition

--- crates/crystallize-core/src/modules/desktops/mod.rs
+++ crates/crystallize-core/src/modules/desktops/mod.rs
@@ -5,13 +5,6 @@ mod services;
 
 pub fn install_desktop_setup(desktop_setup: DesktopSetup) {
   log::debug!("Installing {desktop_setup:?}");
-  match desktop_setup {
-    DesktopSetup::Gnome => desktop::gnome(),
-    DesktopSetup::Kde => desktop::kde(),
-    DesktopSetup::Cinnamon => desktop::cinnamon(),
-    DesktopSetup::None => log::debug!("No desktop setup selected"),
-  }
-
   services::ufw();
   services::networkmanager();
   if desktop_setup != DesktopSetup::None {
@@ -19,5 +12,12 @@ pub fn install_desktop_setup(desktop_setup: DesktopSetup) {
     services::bluetooth();
     services::cups();
     services::tuned_ppd()
+  }
+
+  match desktop_setup {
+    DesktopSetup::Gnome => desktop::gnome(),
+    DesktopSetup::Kde => desktop::kde(),
+    DesktopSetup::Cinnamon => desktop::cinnamon(),
+    DesktopSetup::None => log::debug!("No desktop setup selected"),
   }
 }
